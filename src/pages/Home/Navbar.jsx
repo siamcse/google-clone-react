b599c2746e489a81ade678521802d1feb27409a7
Abu Siam <siamcse01@gmail.com>
2023-08-16 13:27:34 +0600

    navbar added

---
+++ src/pages/Home/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppsIcon from '@mui/icons-material/Apps';
+import PersonIcon from '@mui/icons-material/Person';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='flex justify-end gap-4 mt-5 mr-2'>
+                <p>Gmail</p>
+                <p>Images</p>
+                <p><AppsIcon /></p>
+                <p><PersonIcon /></p>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
