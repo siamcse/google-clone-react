207dca4a2ac7833f00642d2d2d48d2b9e9f2b644
Abu Siam <siamcse01@gmail.com>
2023-08-16 13:29:56 +0600

    Home page done

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import SearchBar from './SearchBar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <SearchBar />
+        </div>
+    );
+};
+
+export default Home;
